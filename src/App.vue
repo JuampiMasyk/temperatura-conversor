971c4ab848e55296313a4d540bff31f62950f151
finalnt2 <juanpablomasyk@gmail.com>
2024-12-17 19:59:40 -0300

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Conversión de Temperatura</h1>
+    <div>
+      <label for="celsius">Ingresa la temperatura en grados Centrígrados:</label>
+      <input 
+        type="number" 
+        id="celsius" 
+        v-model.number="celsius" 
+      />
+    </div>
+    <div>
+      <p :style="{ color: fahrenheitColor }">Fahrenheit: {{ fahrenheit }} °F</p>
+      <p :style="{ color: kelvinColor }">Kelvin: {{ kelvin }} K</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  data() {
+    return {
+      celsius: 0, 
+    };
+  },
+  computed: {
+    
+    fahrenheit() {
+      return (this.celsius * 9/5) + 32;
+    },
+    
+    kelvin() {
+      return this.celsius + 273.15;
+    },
+    
+    fahrenheitColor() {
+      if (this.celsius <= 0) return 'blue';
+      if (this.celsius > 0 && this.celsius < 15) return 'magenta';
+      return 'red';
+    },
+    
+    kelvinColor() {
+      if (this.celsius <= 0) return 'blue';
+      if (this.celsius > 0 && this.celsius < 15) return 'magenta';
+      return 'red';
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+input {
+  padding: 8px;
+  margin: 10px;
+  font-size: 16px;
+  width: 100px;
 }
 </style>
